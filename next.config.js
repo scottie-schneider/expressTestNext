3541667b32947193a41a9dd10f15355702769f32
Scottie Schneider <scott@laclave.co>
2019-12-20 16:44:04 -0500

    config

--- next.config.js
+++ next.config.js
@@ -1,35 +1,31 @@
-// next.config.js
-const withOffline = require('next-offline')
+// Progressive Web App: Add service worker with network-first strategy.
+// Network-first strategy means that if there is no internet connection,
+// the browser will use files previously saved locally to the user’s device instead.
+// AKA Offline Mode!
+const withOffline = require("next-offline");
 
 const nextConfig = {
-    target: 'serverless',
-    env: {
-        // GOOGLE_MAPS: process.env.GOOGLE_MAPS
-    },
-    transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
-    // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
-    // turn on the SW in dev mode so that we can actually test it
-    generateInDevMode: false,
-    workboxOpts: {
-        swDest: 'static/service-worker.js',
-        runtimeCaching: [
-        {
-            urlPattern: /^https?.*/,
-            handler: 'NetworkFirst',
-            options: {
-            cacheName: 'https-calls',
-            networkTimeoutSeconds: 15,
-            expiration: {
-                maxEntries: 150,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-            },
-            cacheableResponse: {
-                statuses: [0, 200],
-            },
-            },
-        },
-        ],
-    },
+  target: "serverless",
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "networkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
 };
-  
-  module.exports = withOffline(nextConfig)
+
+module.exports = withOffline(nextConfig);
